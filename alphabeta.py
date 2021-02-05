f572fb34d3689163ef3af349a28662104f673720
ANAS EL ABEDELALAOUI <72227922+AnasElAbedelalaoui@users.noreply.github.com>
2021-02-05 14:16:00 +0100

    Add files via upload

---
+++ alphabeta.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  4 10:46:45 2021
+
+@author: besbe
+"""
+
---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  4 10:48:54 2021
+
+@author: besbe
+"""
+
